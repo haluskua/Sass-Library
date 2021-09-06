19a71023e46e02c5be949acea13badc9e5384a39
haluskua <haluskua.one@gmail.com>
2021-09-07 11:02:53 +1200

    added project setup intiated

--- gulpfile.js
+++ gulpfile.js
@@ -4,13 +4,13 @@ const sass = require("gulp-sass")(require("sass"));
 // compiling sass into css
 
 function buildStyles() {
-  return src("*.scss").pipe(sass()).pipe(dest("css"));
+  return src("shinobi/**/*.scss").pipe(sass()).pipe(dest("css"));
 }
 
 //ACTIVELY WATCH src file
 
 function watchTask() {
-  watch(["*.scss"], buildStyles);
+  watch(["shinobi/**/*.scss"], buildStyles);
 }
 
 exports.default = series(buildStyles, watchTask);
