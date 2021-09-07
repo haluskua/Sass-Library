a5b152d4ec3db65494fde44f00118a3aec3aa948
haluskua <haluskua.one@gmail.com>
2021-09-07 12:36:56 +1200

    put back scroll snap for page transition

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,9 @@
 const { src, dest, watch, series } = require("gulp");
 const sass = require("gulp-sass")(require("sass"));
 
-// compiling sass into css
-
 function buildStyles() {
   return src("shinobi/**/*.scss").pipe(sass()).pipe(dest("css"));
 }
-
-//ACTIVELY WATCH src file
 
 function watchTask() {
   watch(["shinobi/**/*.scss"], buildStyles);
