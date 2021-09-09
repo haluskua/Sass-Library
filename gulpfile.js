c65b8a63f834f207b1284cf088d8e8c03c543e65
haluskua <haluskua.one@gmail.com>
2021-09-09 22:15:06 +1200

    added Auto prefixer to gulfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,12 @@
 const { src, dest, watch, series } = require("gulp");
 const sass = require("gulp-sass")(require("sass"));
+const prefix = require("gulp-autoprefixer");
 
 function buildStyles() {
-  return src("shinobi/**/*.scss").pipe(sass()).pipe(dest("css"));
+  return src("shinobi/**/*.scss")
+    .pipe(sass())
+    .pipe(prefix(["last 3 versions"], { cascade: true }))
+    .pipe(dest("css"));
 }
 
 function watchTask() {
