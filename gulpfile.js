2fd347724450050bea1533bb473d6e7d9d73fdde
haluskua <haluskua.one@gmail.com>
2021-09-09 22:27:02 +1200

    renamed shinobi folder name to scss

--- gulpfile.js
+++ gulpfile.js
@@ -3,14 +3,14 @@ const sass = require("gulp-sass")(require("sass"));
 const prefix = require("gulp-autoprefixer");
 
 function buildStyles() {
-  return src("shinobi/**/*.scss")
+  return src("scss/**/*.scss")
     .pipe(sass())
     .pipe(prefix(["last 3 versions"], { cascade: true }))
     .pipe(dest("css"));
 }
 
 function watchTask() {
-  watch(["shinobi/**/*.scss"], buildStyles);
+  watch(["scss/**/*.scss"], buildStyles);
 }
 
 exports.default = series(buildStyles, watchTask);
