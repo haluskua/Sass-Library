8ca7dabb2f5acd8980a7cd197cc0554e8f5cf78a
haluskua <haluskua.one@gmail.com>
2021-09-11 12:14:54 +1200

    added css purging plugin through NPM and including *html file in gulpfile to also watch hanges to html for auto purge

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,19 @@
 const { src, dest, watch, series } = require("gulp");
 const sass = require("gulp-sass")(require("sass"));
 const prefix = require("gulp-autoprefixer");
+const purgecss = require("gulp-purgecss");
 
 function buildStyles() {
   return src("scss/**/*.scss")
     .pipe(sass())
     .pipe(prefix(["last 3 versions"], { cascade: true }))
+    .pipe(purgecss({ content: ["*.html"] }))
+
     .pipe(dest("css"));
 }
 
 function watchTask() {
-  watch(["scss/**/*.scss"], buildStyles);
+  watch(["scss/**/*.scss", "*.html"], buildStyles);
 }
 
 exports.default = series(buildStyles, watchTask);
